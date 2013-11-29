3f1570fea9c0313344559403eb10c7690572b74b
ticoerrecart <ticoerrecart@10076d6b-e2e7-1255-1dc2-4db3c410265a>
2013-11-29 19:16:06 +0000

    Alta de Autorizaciones

---
+++ SIIG/WebContent/base/insertTipoDeAutorizacion.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM x071vm20_siig.tipoautorizacion
+LIMIT 0, 1000
+
+-- Date: 2013-11-29 16:14
+*/
+INSERT INTO `tipoautorizacion` (`id`,`descripcion`) VALUES (1,'Solicitud y gestión de Guías de Campaña');
+INSERT INTO `tipoautorizacion` (`id`,`descripcion`) VALUES (2,'Solicitud de Renovación de Títulos');
+INSERT INTO `tipoautorizacion` (`id`,`descripcion`) VALUES (3,'Otra Solicitud');
