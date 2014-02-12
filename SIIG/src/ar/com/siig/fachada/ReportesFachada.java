f4674c0c435d927287ca82a1dd31674fb5b1020c
ticoerrecart <ticoerrecart@10076d6b-e2e7-1255-1dc2-4db3c410265a>
2014-02-12 13:43:44 +0000

    reporte nuevo

--- SIIG/src/ar/com/siig/fachada/ReportesFachada.java
+++ SIIG/src/ar/com/siig/fachada/ReportesFachada.java
@@ -3,59 +3,76 @@ package ar.com.siig.fachada;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import ar.com.siig.dao.ReportesDAO;
 import ar.com.siig.negocio.exception.NegocioException;
 import ar.com.siig.utils.Constantes;
 
-import org.springframework.transaction.annotation.Transactional;
-
 @Transactional(rollbackFor = { Throwable.class })
-public class ReportesFachada{
+public class ReportesFachada {
 
 	private ReportesDAO reportesDAO;
-	
-	public ReportesFachada(){}
-	
-	public ReportesFachada(ReportesDAO pReportesDAO){
-		
-		this.reportesDAO = pReportesDAO;
-	}	
 
-	public byte[] generarReporteVencimientoMarcasSeniales(
-			String path, Long idProductor) throws Exception {
+	public ReportesFachada() {
+	}
+
+	public ReportesFachada(ReportesDAO pReportesDAO) {
+
+		this.reportesDAO = pReportesDAO;
+	}
+
+	public byte[] generarReporteVencimientoMarcasSeniales(String path,
+			Long idProductor) throws Exception {
 
 		Map parameters = new HashMap();
 		parameters.put("PATH_SUB_REPORTES", path);
 		parameters.put("idProductor", idProductor);
 
-		return reportesDAO.generarReporte(Constantes.REPORTE_VENCIMIENTO_MARCAS_SENIALES,parameters);
-	}	
+		return reportesDAO.generarReporte(
+				Constantes.REPORTE_VENCIMIENTO_MARCAS_SENIALES, parameters);
+	}
 
-	public byte[] generarReporteGuias(String tipoGuia,String path, Long idProductor, String periodo) throws Exception {
+	public byte[] generarReporteGuias(String tipoGuia, String path,
+			Long idProductor, String periodo) throws Exception {
 
 		Map parameters = new HashMap();
 		parameters.put("PATH_SUB_REPORTES", path);
 		parameters.put("idProductor", idProductor);
 		parameters.put("periodo", periodo);
 		String nombreReporte = "";
-		if (tipoGuia.equals("Devueltas")){
+		if (tipoGuia.equals("Devueltas")) {
 			nombreReporte = Constantes.REPORTE_GUIAS_DEVUELTAS;
 		} else {
-			if (tipoGuia.equals("Legalizadas")){
+			if (tipoGuia.equals("Legalizadas")) {
 				nombreReporte = Constantes.REPORTE_GUIAS_LEGALIZADAS;
 			} else {
-				if (tipoGuia.equals("Registradas")){
+				if (tipoGuia.equals("Registradas")) {
 					nombreReporte = Constantes.REPORTE_GUIAS_REGISTRADAS;
 				} else {
-					if (tipoGuia.equals("Canceladas")){
+					if (tipoGuia.equals("Canceladas")) {
 						nombreReporte = Constantes.REPORTE_GUIAS_CANCELADAS;
 					} else {
-						throw new NegocioException("No existe el reporte para " + tipoGuia);
-					}	
-				} 
+						throw new NegocioException("No existe el reporte para "
+								+ tipoGuia);
+					}
+				}
 			}
 		}
-		return reportesDAO.generarReporte(nombreReporte,parameters);
-	}	
-	
+		return reportesDAO.generarReporte(nombreReporte, parameters);
+	}
+
+	public byte[] generarReporteProductorPeriodoDestino(String path,
+			Long idProductor, String tipoGuia, String periodo,
+			Long idEstablecimientoDestino) throws Exception {
+
+		Map parameters = new HashMap();
+		parameters.put("PATH_SUB_REPORTES", path);
+		parameters.put("idProductor", idProductor);
+		parameters.put("periodo", periodo);
+		parameters.put("destino", idEstablecimientoDestino);
+		parameters.put("tipoGuia", tipoGuia);
+		return reportesDAO.generarReporte(
+				Constantes.REPORTE_PRODUCTOR_PERIODO_DESTINO, parameters);
+	}
 }
--- SIIG/src/ar/com/siig/struts/actions/ReportesAction.java
+++ SIIG/src/ar/com/siig/struts/actions/ReportesAction.java
@@ -7,15 +7,17 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.springframework.util.StringUtils;
 import org.springframework.web.context.WebApplicationContext;
 
 import ar.com.siig.fachada.EntidadFachada;
+import ar.com.siig.fachada.EstablecimientoFachada;
 import ar.com.siig.fachada.PeriodoFachada;
 import ar.com.siig.fachada.ReportesFachada;
 import ar.com.siig.utils.MyLogger;
 
 public class ReportesAction extends ValidadorAction {
-	
+
 	public ActionForward cargarReporteVencimientoMarcasSeniales(
 			ActionMapping mapping, ActionForm form, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
@@ -23,18 +25,23 @@ public class ReportesAction extends ValidadorAction {
 
 		try {
 			WebApplicationContext ctx = getWebApplicationContext();
-			//PeriodoFachada periodoFachada = (PeriodoFachada) ctx.getBean("periodoFachada");			
-			EntidadFachada entidadFachada = (EntidadFachada) ctx.getBean("entidadFachada");
-			
-			//request.setAttribute("periodos",periodoFachada.getPeriodosDTO());			
-			request.setAttribute("productores",entidadFachada.getProductoresDTO());
-			request.setAttribute("titulo","Reporte Vencimiento de Títulos de Marcas y señales");
-			request.setAttribute("action","reportes");
-			request.setAttribute("metodo","generarReporteVencimientoMarcasSeniales");
-			request.setAttribute("permitirTodosLosProductores","S");
-			//request.setAttribute("permitirTodosLosPeriodos","N");
-			request.setAttribute("mostrarPeriodos","N");
-			
+			// PeriodoFachada periodoFachada = (PeriodoFachada)
+			// ctx.getBean("periodoFachada");
+			EntidadFachada entidadFachada = (EntidadFachada) ctx
+					.getBean("entidadFachada");
+
+			// request.setAttribute("periodos",periodoFachada.getPeriodosDTO());
+			request.setAttribute("productores",
+					entidadFachada.getProductoresDTO());
+			request.setAttribute("titulo",
+					"Reporte Vencimiento de Títulos de Marcas y señales");
+			request.setAttribute("action", "reportes");
+			request.setAttribute("metodo",
+					"generarReporteVencimientoMarcasSeniales");
+			request.setAttribute("permitirTodosLosProductores", "S");
+			// request.setAttribute("permitirTodosLosPeriodos","N");
+			request.setAttribute("mostrarPeriodos", "N");
+
 		} catch (Throwable t) {
 			MyLogger.logError(t);
 			request.setAttribute("error", "Error Inesperado");
@@ -42,7 +49,7 @@ public class ReportesAction extends ValidadorAction {
 		}
 		return mapping.findForward(strForward);
 	}
-	
+
 	public ActionForward generarReporteVencimientoMarcasSeniales(
 			ActionMapping mapping, ActionForm form, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
@@ -52,10 +59,13 @@ public class ReportesAction extends ValidadorAction {
 					.getRealPath("jasper");
 
 			WebApplicationContext ctx = getWebApplicationContext();
-			ReportesFachada reportesFachada = (ReportesFachada) ctx.getBean("reportesFachada");
+			ReportesFachada reportesFachada = (ReportesFachada) ctx
+					.getBean("reportesFachada");
 			String productor = request.getParameter("productor");
-			
-			byte[] bytes = reportesFachada.generarReporteVencimientoMarcasSeniales(path,Long.valueOf(productor));
+
+			byte[] bytes = reportesFachada
+					.generarReporteVencimientoMarcasSeniales(path,
+							Long.valueOf(productor));
 
 			// Lo muestro en la salida del response
 			response.setContentType("application/pdf");
@@ -71,41 +81,46 @@ public class ReportesAction extends ValidadorAction {
 
 		return null;
 	}
-	
-	public ActionForward cargarReporteGuias(
-			ActionMapping mapping, ActionForm form, HttpServletRequest request,
+
+	public ActionForward cargarReporteGuias(ActionMapping mapping,
+			ActionForm form, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		String strForward = "exitoCargarReporteGuias";
 
 		try {
 			WebApplicationContext ctx = getWebApplicationContext();
-			PeriodoFachada periodoFachada = (PeriodoFachada) ctx.getBean("periodoFachada");			
-			EntidadFachada entidadFachada = (EntidadFachada) ctx.getBean("entidadFachada");
-			
-			String tipoGuia =  request.getParameter("tipoGuia");
+			PeriodoFachada periodoFachada = (PeriodoFachada) ctx
+					.getBean("periodoFachada");
+			EntidadFachada entidadFachada = (EntidadFachada) ctx
+					.getBean("entidadFachada");
+
+			String tipoGuia = request.getParameter("tipoGuia");
 			if (tipoGuia.equals("Legalizadas")) {
-				request.setAttribute("titulo","Reporte Guías Legalizadas");
+				request.setAttribute("titulo", "Reporte Guías Legalizadas");
 			} else {
 				if (tipoGuia.equals("Registradas")) {
-					request.setAttribute("titulo","Reporte Guías Registradas");
+					request.setAttribute("titulo", "Reporte Guías Registradas");
 				} else {
 					if (tipoGuia.equals("Devueltas")) {
-						request.setAttribute("titulo","Reporte Producto Movido");
+						request.setAttribute("titulo",
+								"Reporte Producto Movido");
 					} else {
 						if (tipoGuia.equals("Canceladas")) {
-							request.setAttribute("titulo","Reporte Guías Canceladas");
+							request.setAttribute("titulo",
+									"Reporte Guías Canceladas");
 						}
-					}	
+					}
 				}
 			}
-			request.setAttribute("periodos",periodoFachada.getPeriodosDTO());			
-			request.setAttribute("productores",entidadFachada.getProductoresDTO());
-			request.setAttribute("action","reportes");
-			request.setAttribute("metodo","generarReporteGuias");
-			request.setAttribute("tipoGuia",tipoGuia);
-			request.setAttribute("permitirTodosLosProductores","S");
-			request.setAttribute("permitirTodosLosPeriodos","S");			
-			
+			request.setAttribute("periodos", periodoFachada.getPeriodosDTO());
+			request.setAttribute("productores",
+					entidadFachada.getProductoresDTO());
+			request.setAttribute("action", "reportes");
+			request.setAttribute("metodo", "generarReporteGuias");
+			request.setAttribute("tipoGuia", tipoGuia);
+			request.setAttribute("permitirTodosLosProductores", "S");
+			request.setAttribute("permitirTodosLosPeriodos", "S");
+
 		} catch (Throwable t) {
 			MyLogger.logError(t);
 			request.setAttribute("error", "Error Inesperado");
@@ -113,9 +128,9 @@ public class ReportesAction extends ValidadorAction {
 		}
 		return mapping.findForward(strForward);
 	}
-	
-	public ActionForward generarReporteGuias(
-			ActionMapping mapping, ActionForm form, HttpServletRequest request,
+
+	public ActionForward generarReporteGuias(ActionMapping mapping,
+			ActionForm form, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 
 		try {
@@ -123,11 +138,13 @@ public class ReportesAction extends ValidadorAction {
 					.getRealPath("jasper");
 
 			WebApplicationContext ctx = getWebApplicationContext();
-			ReportesFachada reportesFachada = (ReportesFachada) ctx.getBean("reportesFachada");
+			ReportesFachada reportesFachada = (ReportesFachada) ctx
+					.getBean("reportesFachada");
 			String productor = request.getParameter("productor");
 			String periodo = request.getParameter("periodo");
-			String tipoGuia =  request.getParameter("tipoGuia");
-			byte[] bytes = reportesFachada.generarReporteGuias(tipoGuia, path,Long.valueOf(productor),periodo);
+			String tipoGuia = request.getParameter("tipoGuia");
+			byte[] bytes = reportesFachada.generarReporteGuias(tipoGuia, path,
+					Long.valueOf(productor), periodo);
 
 			// Lo muestro en la salida del response
 			response.setContentType("application/pdf");
@@ -142,5 +159,80 @@ public class ReportesAction extends ValidadorAction {
 		}
 
 		return null;
-	}	
+	}
+
+	public ActionForward cargarReporteProductorPeriodoDestino(
+			ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		String strForward = "exitoCargarReporteProductorPeriodoDestino";
+
+		try {
+			WebApplicationContext ctx = getWebApplicationContext();
+			PeriodoFachada periodoFachada = (PeriodoFachada) ctx
+					.getBean("periodoFachada");
+			EntidadFachada entidadFachada = (EntidadFachada) ctx
+					.getBean("entidadFachada");
+			EstablecimientoFachada establecimientoFachada = (EstablecimientoFachada) ctx
+					.getBean("establecimientoFachada");
+
+			String tipoGuia = request.getParameter("tipoGuia");
+			if (!StringUtils.hasText(tipoGuia)) {
+				tipoGuia = "DEVUELTA";
+			}
+			request.setAttribute("titulo",
+					"Reporte Detalle Guías Por Establecimiento Destino");
+			request.setAttribute("periodos", periodoFachada.getPeriodosDTO());
+			request.setAttribute("productores",
+					entidadFachada.getProductoresDTO());
+			request.setAttribute("destinos",
+					establecimientoFachada.getEstablecimientosDTO());
+			request.setAttribute("action", "reportes");
+			request.setAttribute("metodo", "generarProductorPeriodoDestino");
+			request.setAttribute("tipoGuia", tipoGuia);
+			request.setAttribute("permitirTodosLosProductores", "S");
+			request.setAttribute("permitirTodosLosPeriodos", "S");
+			request.setAttribute("permitirTodosLosEstablecimientos", "S");
+
+		} catch (Throwable t) {
+			MyLogger.logError(t);
+			request.setAttribute("error", "Error Inesperado");
+			strForward = "error";
+		}
+		return mapping.findForward(strForward);
+	}
+
+	public ActionForward generarProductorPeriodoDestino(ActionMapping mapping,
+			ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+
+		try {
+			String path = request.getSession().getServletContext()
+					.getRealPath("jasper");
+
+			WebApplicationContext ctx = getWebApplicationContext();
+			ReportesFachada reportesFachada = (ReportesFachada) ctx
+					.getBean("reportesFachada");
+			String productor = request.getParameter("productor");
+			String periodo = request.getParameter("periodo");
+			String tipoGuia = request.getParameter("tipoGuia");
+			String destino = request.getParameter("establecimientoDestino");
+			byte[] bytes = reportesFachada
+					.generarReporteProductorPeriodoDestino(path,
+							Long.valueOf(productor), tipoGuia, periodo,
+							Long.valueOf(destino));
+
+			// Lo muestro en la salida del response
+			response.setContentType("application/pdf");
+			ServletOutputStream out = response.getOutputStream();
+			out.write(bytes, 0, bytes.length);
+			out.flush();
+
+		} catch (Throwable t) {
+			MyLogger.logError(t);
+			request.setAttribute("error", "Error Inesperado");
+			return mapping.findForward("errorSinMenu");
+		}
+
+		return null;
+	}
 }
